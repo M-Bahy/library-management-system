a82fba87599986e9dc429f091c4b52bc1558712a
M-Bahy <Homelander.SolderBoy@gmail.com>
2023-07-11 22:16:42 +0300

    test book done

--- src/tests/test_book.py
+++ src/tests/test_book.py
@@ -5,43 +5,42 @@ sys.path.append("src\main")
 from book import Book
 
 
-class TestBook(unittest.TestCase):
-    def test_get_title(self):
+def bahy(f):
+    def wrapper(*args, **kwargs):
         b = Book(
             "The Catcher in the Rye", ["J. D. Salinger"], 10, "0316769177"
         )
+        f(*args, b, **kwargs)
+
+    return wrapper
+
+
+class TestBook(unittest.TestCase):
+    @bahy
+    def test_get_title(self, b):
         self.assertEqual(b.get_title(), "The Catcher in the Rye")
 
-    def test_get_authors(self):
-        b = Book(
-            "The Catcher in the Rye", ["J. D. Salinger"], 10, "0316769177"
-        )
+    @bahy
+    def test_get_authors(self, b):
         self.assertEqual(b.get_authors(), ["J. D. Salinger"])
 
-    def test_get_price(self):
-        b = Book(
-            "The Catcher in the Rye", ["J. D. Salinger"], 10, "0316769177"
-        )
+    @bahy
+    def test_get_price(self, b):
         self.assertEqual(b.get_price(), 10)
 
-    def test_get_isbn(self):
-        b = Book(
-            "The Catcher in the Rye", ["J. D. Salinger"], 10, "0316769177"
-        )
+    @bahy
+    def test_get_isbn(self, b):
         self.assertEqual(b.get_isbn(), "0316769177")
 
-    def test_get_availability(self):
-        b = Book(
-            "The Catcher in the Rye", ["J. D. Salinger"], 10, "0316769177"
-        )
+    @bahy
+    def test_get_availability(self, b):
         self.assertEqual(b.get_availability(), True)
 
-    def test_set_availability(self):
-        b = Book(
-            "The Catcher in the Rye", ["J. D. Salinger"], 10, "0316769177"
-        )
+    @bahy
+    def test_set_availability(self, b):
         b.set_availability(False)
         self.assertEqual(b.get_availability(), False)
 
 
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
